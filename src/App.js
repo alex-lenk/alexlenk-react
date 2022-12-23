9d751b21b3efa7c40e201e6e85174ea4373acc76
AlexLenk <alexlenk.ru@gmail.com>
2022-12-23 08:18:49 +0300

    Added route and created home page

--- src/App.js
+++ src/App.js
@@ -3,18 +3,20 @@ import './assets/scss/styles.scss'
 import logo from './assets/img/logo/logo-1.svg'
 import logoTwo from './assets/img/logo/logo-2.svg'
 import Panel from './components/Panel'
-import NavMenu from "./components/NavMenu";
+import NavMenu from './components/NavMenu'
+import Home from './pages/Home'
+import {BrowserRouter, Route, Switch} from 'react-router-dom'
 
 function App() {
   const siteName = 'Александр Янковой - Frontend developer'
   return (
-    <>
+    <BrowserRouter>
       <Panel logo={logoTwo} siteName={siteName}/>
       <NavMenu logo={logo} siteName={siteName}/>
-      <div className="page__content container">
-        App
-      </div>
-    </>
+      <Switch>
+        <Route path='/' exact component={Home}/>
+      </Switch>
+    </BrowserRouter>
   )
 }
 
---
+++ src/pages/Home.jsx
@@ -0,0 +1,78 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div className="page__content container">
+      <div className="home js-home">
+        <div className="home__content">
+          <div className="home__content-name">Александр Янковой</div>
+
+          <h1 className="home__content-title">Frontend developer</h1>
+
+          <div className="home__content-phrase">
+            Пишу легкий, чистый
+            <span className="home__content-decor">&lt;code&gt;</span>
+
+            <div className="home__content-text" data-decor="// ">
+              Чтобы ваш сайт работал быстро и&nbsp;на&nbsp;полную.
+            </div>
+          </div>
+
+          <a href="#" className="home__content-btn js-home__content-btn btn__primary btn">Портфолио</a>
+        </div>
+
+        <aside className="home__card">
+          <div className="home__card-top">
+            <div className="home__card-projects">96 projects</div>
+            <div className="home__card-info">1 455 commits</div>
+          </div>
+
+          <div className="home__card-container">
+            <div className="home__card-code">
+              <div className="code-tag">&#60;header&#62;</div>
+              <div>
+                &nbsp;&nbsp;<span className="code-tag">&#60;div id=</span><span className="code-id">&quot;coder&quot;</span><span className="code-tag">&#62;</span>
+              </div>
+              <div>&nbsp;&nbsp;&nbsp;&nbsp;
+                <span className="code-tag">&#60;h1&#62;</span><span className="code-title"> Technology stack </span><span className="code-tag">&#60;/h1&#62;</span>
+              </div>
+              <div className="code-tag">&nbsp;&nbsp;&nbsp;&nbsp;&#60;ul&#62;</div>
+              <div>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                <span className="code-tag">&#60;li&#62;</span> <span className="code-text">HTML</span> <span  className="code-tag">&#60;/li&#62;</span>
+              </div>
+              <div>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                <span className="code-tag">&#60;li&#62;</span> <span className="code-text">CSS</span> <span className="code-tag">&#60;/li&#62;</span>
+              </div>
+              <div>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                <span className="code-tag">&#60;li&#62;</span> <span className="code-text">JavaScript</span> <span className="code-tag">&#60;/li&#62;</span>
+              </div>
+              <div>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                <span className="code-tag">&#60;li&#62;</span> <span className="code-text">React.js</span> <span className="code-tag">&#60;/li&#62;</span>
+              </div>
+              <div>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                <span className="code-tag">&#60;li&#62;</span> <span className="code-text">Vue.js</span> <span className="code-tag">&#60;/li&#62;</span>
+              </div>
+              <div>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                <span className="code-tag">&#60;li&#62;</span> <span className="code-text">Node.js</span> <span className="code-tag">&#60;/li&#62;</span>
+              </div>
+              <div>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                <span className="code-tag">&#60;li&#62;</span> <span className="code-text">Webpack.js</span> <span className="code-tag">&#60;/li&#62;</span>
+              </div>
+              <div>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                <span className="code-tag">&#60;li&#62;</span> <span className="code-text">Gulp.js</span> <span className="code-tag">&#60;/li&#62;</span>
+              </div>
+              <div>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                <span className="code-tag">&#60;li&#62;</span> <span className="code-text">Git</span> <span className="code-tag">&#60;/li&#62;</span>
+              </div>
+              <div className="code-tag">&nbsp;&nbsp;&nbsp;&nbsp;&#60;/ul&#62;</div>
+              <div className="code-tag">&nbsp;&nbsp;&#60;/div&#62;</div>
+              <div className="code-tag">&#60;/header&#62;</div>
+            </div>
+          </div>
+        </aside>
+      </div>
+    </div>
+  )
+}
+
+export default Home
